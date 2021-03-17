059e25732f27074ca4fc84abe85fded19e6481a9
Thomas Mora <thomas.mora@est.fi.uncoma.edu.ar>
2021-03-17 16:15:22 -0300

    Update vinotecaNqn.php
    POR FIN ENTENDIMOS LO QUE HABIA QUE HACER

--- vinotecaNqn.php
+++ vinotecaNqn.php
@@ -3,9 +3,22 @@
 function getVinoteca()
 {
     $vinoteca = array();
-    $vinoteca["Malbec"] = array("stock" => "250", "anioProduccion" => "2005", "precioUnitario" => "300");
-    $vinoteca["Cabernet Sauvignon"] = array("stock" => "150", "anioProduccion" => "2001", "precioUnitario" => "700");
-    $vinoteca["Merlot"] = array("stock" => "200", "anioProduccion" => "2003", "precioUnitario" => "500");
+
+    $malbec[0] = array("stock" => "2505", "anioProduccion" => "2005", "precioUnitario" => "300");
+    $malbec[1] = array("stock" => "150", "anioProduccion" => "2002", "precioUnitario" => "600");
+    $malbec[2] = array("stock" => "50", "anioProduccion" => "1999", "precioUnitario" => "1600");
+
+    $cs[0] = array("stock" => "150", "anioProduccion" => "2008", "precioUnitario" => "700");
+    $cs[1] = array("stock" => "100", "anioProduccion" => "2005", "precioUnitario" => "2700");
+    $cs[2] = array("stock" => "10", "anioProduccion" => "2001", "precioUnitario" => "5700");
+
+    $merlot[0] = array("stock" => "250", "anioProduccion" => "2003", "precioUnitario" => "500");
+    $merlot[1] = array("stock" => "210", "anioProduccion" => "2002", "precioUnitario" => "600");
+    $merlot[2] = array("stock" => "200", "anioProduccion" => "2001", "precioUnitario" => "700");
+
+    $vinoteca["Malbec"] = $malbec;
+    $vinoteca["Cabernet Sauvignon"] = $cs;
+    $vinoteca["Merlot"] = $merlot;
     return $vinoteca;
 }
 
@@ -14,7 +27,5 @@ function main()
     $vinoteca = getVinoteca();
     print_r($vinoteca);
 }
-
-
 
 main();
