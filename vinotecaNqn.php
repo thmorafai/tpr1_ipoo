aa7cec081bf6cf18d216ba40a2da5cc496440c07
Thomas Mora <thomas.mora@est.fi.uncoma.edu.ar>
2021-03-17 15:47:08 -0300

    Update vinotecaNqn.php
    lei de nuevo el enunciado y habia hecho todo en el main, mala mia

--- vinotecaNqn.php
+++ vinotecaNqn.php
@@ -1,13 +1,20 @@
 <?php
 
-function main()
+function getArray()
 {
     $vinos = array();
     $vinos[0] = array("variedad" => "Malbec", "cantidadBotellas" => "250", "anioProduccion" => "2005", "precioUnitario" => "300");
     $vinos[1] = array("variedad" => "Cabernet Sauvignon", "cantidadBotellas" => "150", "anioProduccion" => "2001", "precioUnitario" => "700");
     $vinos[2] = array("variedad" => "Merlot", "cantidadBotellas" => "200", "anioProduccion" => "2003", "precioUnitario" => "500");
+    return $vinos;
+}
+
+function main()
+{
+    $vinos = getArray();
     print_r($vinos);
 }
+
 
 
 main();
